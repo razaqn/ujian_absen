791654518cf8e686c7fdb86981d20860fc16be1d
Abdur Razaq Nawfal <razaqnawfal@gmail.com>
2022-12-19 11:07:36 +0700

    create_user

---
+++ resources/views/backend/user/create.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('title')
+    User | Create
+@endsection
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('User | Create') }}</div>
+                <div class="card-body">
+                    <form id="contactForm" action="{{ route('backend.create.process.user') }}" method="post" enctype="multipart/form-data">
+                        @csrf
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-12 col-md-12 mb-3">
+
+                                <div class="mb-3">
+                                    <div class="mb-2 @error('name') text-danger fw-bold @enderror">Name:</div>
+                                    <input type="text" name="name" value="{{ old('name') }}" placeholder="Name" 
+                                    class="form-control @error('name') text-danger is-invalid @enderror">
+                                    @error('name')
+                                        <small class="text-danger">{!! $message !!}</small>
+                                    @enderror
+                                </div>
+
+                                <div class="mb-3">
+                                    <div class="mb-2 @error('slug') text-danger fw-bold @enderror">Slug:</div>
+                                    <input type="text" name="slug" value="{{ old('slug') }}" placeholder="Slug" 
+                                    class="form-control @error('slug') text-danger is-invalid @enderror">
+                                    @error('slug')
+                                        <small class="text-danger">{!! $message !!}</small>
+                                    @enderror
+                                </div>
+
+                                <button class="btn btn-dark">Create</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
