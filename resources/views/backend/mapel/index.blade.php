689aa08a1c7f5835764e09362324fad72ae08139
Satriauno <satria.uno@gmail.com>
2022-12-20 11:37:27 +0700

    up

--- resources/views/backend/mapel/index.blade.php
+++ resources/views/backend/mapel/index.blade.php
@@ -7,6 +7,14 @@
 @section('javascript')
     <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap5.min.js"></script>
+
+    <script>
+        $(document).ready(function () {
+            $('table').DataTable({
+                "pageLength" : 10
+            });
+        });
+    </script>
 @endsection
 
 @section('css')
