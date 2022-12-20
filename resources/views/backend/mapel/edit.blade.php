34ded879101443c41a9643a536fec89c493c5fb3
Satriauno <satria.uno@gmail.com>
2022-12-20 11:35:19 +0700

    changes

--- resources/views/backend/mapel/edit.blade.php
+++ resources/views/backend/mapel/edit.blade.php
@@ -38,7 +38,7 @@
                                         <label for="name" class="form-label">
                                             Name <span class="text-danger">*</span>
                                         </label>
-                                        <input type="text" name="name" value="{{ old('name') }}" placeholder="Name" class="form-control @error('name') is-invalid @enderror">
+                                        <input type="text" name="name" value="{{ $mapel->name }}" placeholder="Name" class="form-control @error('name') is-invalid @enderror">
                                         @error('name')
                                             <small class="text-danger">{!! $message !!}</small>
                                         @enderror
@@ -47,7 +47,7 @@
                                         <label for="slug" class="form-label">
                                             Slug <span class="text-danger">*</span>
                                         </label>
-                                        <input type="text" name="slug" value="{{ old('slug') }}" placeholder="Slug" class="form-control @error('slug') is-invalid @enderror">
+                                        <input type="text" name="slug" value="{{ $mapel->slug }}" placeholder="Slug" class="form-control @error('slug') is-invalid @enderror">
                                         @error('slug')
                                             <small class="text-danger">{!! $message !!}</small>
                                         @enderror
